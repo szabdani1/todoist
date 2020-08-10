a5db1264ac1985db7a5292475a85d83b892ef73c
szabdani1 <34173432+szabdani1@users.noreply.github.com>
2020-08-10 19:14:24 +0200

    Build

---
+++ build/precache-manifest.58b76499f4f29cdf22aae5c2dbd60358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcc2ca7e2cde574c2e55b1093426e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "341d36d2953731e718e3",
+    "url": "/static/css/main.ad1b8bc6.chunk.css"
+  },
+  {
+    "revision": "e065284fdf12973d8681",
+    "url": "/static/js/2.392aff5f.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.392aff5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341d36d2953731e718e3",
+    "url": "/static/js/main.abcb1f2e.chunk.js"
+  },
+  {
+    "revision": "4a9b8479888021b113cf",
+    "url": "/static/js/runtime-main.4fa4db41.js"
+  }
+]);
